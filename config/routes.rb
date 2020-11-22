5f65c9604ea93a518a0e09b6059287e01ecb5e80
Santiago Torres G <santiago.x.torres.-nd@disney.com>
2020-11-21 21:00:26 -0500

    Update Routes for statistics with postgis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :restaurants
+      resources :restaurants, except: %i[edit, new]
+      get 'statistics', action: :gis, controller: 'statistics'
     end
   end
 end
