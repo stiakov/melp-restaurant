b297528558a545640a455b7414e9e3207a59821f
Santiago Torres G <santiago.x.torres.-nd@disney.com>
2020-11-22 17:22:53 -0500

    Add route for restaurant/statistics

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :restaurants, except: %i[edit, new]
-      get 'statistics', action: :gis, controller: 'statistics'
+      resources :restaurants, except: %i[edit, new, show]
+
+      get 'restaurants/show/:id', action: :show, controller: 'restaurants'
+      get 'restaurants/statistics', action: :gis, controller: 'statistics'
     end
   end
 end
